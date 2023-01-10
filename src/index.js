ca566195aa31c053684961ad8c1870791182a373
carlos <carlosjuniorrvega@hotmail.com>
2023-01-09 19:55:11 -0500

    feat: test. CV

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 //Set port
 app.set('port', process.env.PORT || 3000);
-//Middlewares
+//Middlewares s
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
